25653075b536f1972a5566e741e0ed97ebd82a5c
NgoDuc <ngoduc202@gmail.com>
2023-03-20 10:37:35 +0700

    upload: responsive nav-bar + cart model

---
+++ customer/app/services/cartSide.js
@@ -0,0 +1,12 @@
+function cartOpen(){
+    document.querySelector('.cartBox').classList.toggle("cartClose")
+    document.querySelector(".cartBlur").classList.toggle("blurDisplay")
+}
+document.querySelector(".cartLink").addEventListener('click',cartOpen)
+
+
+function cartBlur(){
+    cartOpen()
+    document.querySelector(".cartBlur").classList.add("blurDisplay")
+}
+document.querySelector(".cartBlur").addEventListener('click',cartBlur)
